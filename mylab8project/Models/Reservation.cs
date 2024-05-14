a54f538ed1a3f87567630ff8eb8a280669d45c32
zeyneprsl <120337269+zeyneprsl@users.noreply.github.com>
2024-05-14 14:38:48 +0300

    models,uml added

---
+++ mylab8project/Models/Reservation.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace mylab8project.Models
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public DateTime ReservationDate { get; set; }
+        // Diğer özellikleri buraya ekleyin
+    }
+}
